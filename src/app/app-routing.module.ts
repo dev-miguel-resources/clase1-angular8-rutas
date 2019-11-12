5c7c8c4273e78dd169a6ac0bfc6fefcd637f983c
dev-miguel-resources <ingenieromiguelch@gmail.com>
2019-11-11 21:55:48 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
+import { SubdComponent } from './compa/subd/subd.component';
+import { SubcComponent } from './subc/subc.component';
+import { SubbComponent } from './subb/subb.component';
+import { SubaComponent } from './suba/suba.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompcComponent } from './compc/compc.component';
+import { CompbComponent } from './compb/compb.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // compa/subb
+    // compa/subc
+    path: 'compa', component: CompaComponent, children: [
+      { path: 'subb', component: SubbComponent },
+      { path: 'subc', component: SubcComponent },
+      { path: 'subd', component: SubdComponent }
+    ]
+  },
+  { path: 'compb', component: CompbComponent },
+  { path: 'compc', component: CompcComponent },
+  { path: 'suba', component: SubaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; //importamos la clase exportable de rutas
 import { AppComponent } from './app.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CompcComponent } from './compc/compc.component';
+import { SubaComponent } from './suba/suba.component';
+import { SubbComponent } from './subb/subb.component';
+import { SubcComponent } from './subc/subc.component';
+import { SubdComponent } from './compa/subd/subd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaComponent,
+    CompbComponent,
+    CompcComponent,
+    SubaComponent,
+    SubbComponent,
+    SubcComponent,
+    SubdComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule //acá se registra el módulo de rutas
   ],
   providers: [],
   bootstrap: [AppComponent]
